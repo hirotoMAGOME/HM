1d637e7c64808550c5e6de57778c0d38fd715d85
Unknown <soma.dev.magome@gmail.com>
2019-03-27 23:50:56 +0900

    20190327

--- hiroto/views.py
+++ hiroto/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
-# Create your views here.
+from .mixin import *
+from django.shortcuts import render
 
+
+#class IndexView(LoginRequiredMixin,TemplateView):
 class IndexView(TemplateView):
+    def get(self,request):
+        print(vars(request.session))
     template_name = 'hiroto/index.html'
+
 index = IndexView.as_view()
