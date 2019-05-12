d26df72e17743c52aad8d51c14b80ec6db2d363c
Unknown <soma.dev.magome@gmail.com>
2019-05-13 01:14:37 +0900

    insert PaymentResult

--- budget/views.py
+++ budget/views.py
@@ -58,6 +58,43 @@ class IndexView(View):
 
     def post(self, request, *args, **kwargs):
         """POST リクエスト用のメソッド"""
-        
+        print(request.POST)
+        print(request.POST['memo'])
+        #TODO payment_plan_id,amount_plus_flg,family_id,member_id,rank,payment_date
+        insert = {
+            'payment_plan_id': 1,
+            'amount_plus_flg': 1,
+            'amount': request.POST['amount'],
+            'memo': request.POST['memo'],
+            'family_id': 1,
+            'member_id': 1,
+            'rank': 1,
+            'payment_date': request.POST['payment_date'],
+        }
+        PaymentResult.objects.create(**insert)
+
+        #表示用
+        payment_result_data1 = PaymentResult.objects.select_related('payment_plan').filter(payment_plan__payment_unit_id=1).select_related('payment_plan__payment_unit').all()
+        payment_result_data2 = PaymentResult.objects.select_related('payment_plan').filter(payment_plan__payment_unit_id=2).select_related('payment_plan__payment_unit').all()
+        payment_result_data3 = PaymentResult.objects.select_related('payment_plan').filter(payment_plan__payment_unit_id=3).select_related('payment_plan__payment_unit').all()
+        payment_result_data4 = PaymentResult.objects.select_related('payment_plan').filter(payment_plan__payment_unit_id=4).select_related('payment_plan__payment_unit').all()
+        payment_result_data5 = PaymentResult.objects.select_related('payment_plan').filter(payment_plan__payment_unit_id=5).select_related('payment_plan__payment_unit').all()
+
+        context = {
+            'payment_unit_data1': payment_result_data1,
+            'payment_unit_data2': payment_result_data2,
+            'payment_unit_data3': payment_result_data3,
+            'payment_unit_data4': payment_result_data4,
+            'payment_unit_data5': payment_result_data5,
+            'payment_unit_count1': len(payment_result_data1),
+            'payment_unit_count2': len(payment_result_data2),
+            'payment_unit_count3': len(payment_result_data3),
+            'payment_unit_count4': len(payment_result_data4),
+            'payment_unit_count5': len(payment_result_data5),
+            'resultForm': PaymentResultForm(),
+        }
+
+        return render(request, 'budget/index.html', context)
+
 
 index = IndexView.as_view()
